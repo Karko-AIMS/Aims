da18f86d2b9fd06a4d9497499b85d738adc40f3a
JongminChoi98 <karcjm01@karko.co.kr>
2026-02-02 11:11:08 +0900

    chore(wpf): add WPF client project and base API config

---
+++ Aims.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Aims.Wpf.Services;
+
+namespace Aims.Wpf;
+
+public partial class MainWindow
+{
+    private readonly ApiClient _api = new();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Aims.Wpf/Services/ApiClient.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Net.Http;
+
+namespace Aims.Wpf.Services;
+
+public sealed class ApiClient
+{
+    public HttpClient Http { get; }
+
+    public ApiClient()
+    {
+        var baseUrl = ConfigurationManager.AppSettings["ApiBaseUrl"]
+                      ?? throw new InvalidOperationException("Missing ApiBaseUrl in App.config");
+
+        Http = new HttpClient
+        {
+            BaseAddress = new Uri(baseUrl.TrimEnd('/') + "/")
+        };
+    }
+}
